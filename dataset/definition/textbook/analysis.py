5136210624aadcd5cee2b2a327c49be9bccde090
amir pouran ben veyseh <apouranb@legendary1.cs.uoregon.edu>
2019-08-07 11:19:11 -0700

    a

--- dataset/definition/textbook/analysis.py
+++ dataset/definition/textbook/analysis.py
@@ -1,5 +1,8 @@
 import json
 from collections import Counter
+import spacy
+
+nlp = spacy.load("en")
 
 with open('dataset2.json') as file:
     dataset = json.load(file)
@@ -14,7 +17,13 @@ for d in dataset:
             if 'Term' in l:
                 term += d['tokens'][i]+" "
     if len(term) > 0:
-        terms.append(term)
+        final_term = ''
+        term = nlp(term)
+        for token in term:
+            if not token.is_stop:
+                final_term += token.text + ' '
+        if len(final_term) > 0:
+            terms.append(final_term)
 
 for term in terms:
     print(term)
