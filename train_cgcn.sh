9d22fa1e5e6c8384dd389596c0157ff8c3a84f03
amir pouran ben veyseh <apouranb@legendary1.cs.uoregon.edu>
2019-08-17 01:27:32 -0700

    a

--- train_cgcn.sh
+++ train_cgcn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SAVE_ID=$1
-CUDA_VISIBLE_DEVICES=2 python3 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.0003 --optim adamax --rnn_hidden 200 --num_epoch 100 --pooling max --mlp_layers 2 --pooling_l2 0.003 --consistency_loss 1.0 --data_dir dataset/definition/lca --vocab_dir dataset/vocab
+CUDA_VISIBLE_DEVICES=3 python3 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.0003 --optim adamax --rnn_hidden 200 --num_epoch 100 --pooling max --mlp_layers 2 --pooling_l2 0.003 --consistency_loss 1.0 --sent_loss 0 --dep_path_loss 0 --consistency_loss 0 
