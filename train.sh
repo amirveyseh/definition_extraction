bfde9243f7150c58002489a161b2cc1a3db00a7a
amirveyseh <amirveyseh@gmail.com>
2019-11-08 13:25:45 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SAVE_ID=$1
-CUDA_VISIBLE_DEVICES=3 python3 train.py --id $SAVE_ID --seed 0 --lr 0.0003 --optim adamax --rnn_hidden 200 --num_epoch 100 --pooling max --mlp_layers 2 --pooling_l2 0.003 --consistency_loss 1.0 --sent_loss 100 --dep_path_loss 100 --consistency_loss 1
+python3 train.py --id $SAVE_ID --seed 0 --lr 0.0003 --optim adamax --rnn_hidden 200 --num_epoch 100 --pooling max --mlp_layers 2 --pooling_l2 0.003 --consistency_loss 1.0 --sent_loss 100 --dep_path_loss 100 --consistency_loss 1
 
